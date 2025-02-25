6e8f87f6590fbbf2c7c3e62fc9362f69a9af8a41
Carlos <carlos_aperdomo@soy.sena.edu.co>
2025-02-24 22:39:55 -0500

    se comento axos en el archivo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 
-window.axios = axios
+/*window.axios = axios
 
 window.axios.defaults.baseURL = 'https://reqres.in/api'
 window.axios.defaults.headers.common['Accept'] = 'application/json'
 window.axios.defaults.headers.common['Content-Type'] = 'application/json'
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.whithCredentials = true
+*/
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
